e01bb35b1ed8f5f9871e4f291bec26833bad7fc7
Ashley How <28279067+AshleyHow@users.noreply.github.com>
2022-01-11 19:05:30 +0000

    Add files via upload

--- WindowsOSBuild.psm1
+++ WindowsOSBuild.psm1
@@ -350,15 +350,76 @@ Function Get-LatestOSBuild {
 Function Get-CurrentOSBuild {
     <#
         .SYNOPSIS
-            Gets the currently installed OS Build release number for Windows 10 including Windows Server versions.
+            Gets the currently installed OS Build release information. Supports Windows 10 and Windows Server 2016 onwards.
         .DESCRIPTION
-            Installed OS Build release number is obtained from the registry.
+            Installed OS Build number or detailed information (Version, Build, Availability date, Preview, Out-of-band, Servicing option, KB article, KB URL and Catalog URL).
+        .PARAMETER Detailed
+            This parameter is optional. Returns detailed information about the installed OS Build.
         .EXAMPLE
-            C:\PS> Get-LatestOSBuild -OSVersion 21H1
-            Will show all information on the latest available OS Build information for Version 21H1 in list format.
+            Get-CurrentOSBuild
+            Show only the build number for the installed OS Build.
+        .EXAMPLE
+            Get-CurrentOSBuild -Detailed
+            Show detailed information for the installed OS Build.
     #>
+
+    Param(
+        [CmdletBinding()]
+        [Parameter(Mandatory = $false)]
+        [Switch]$Detailed
+    )
+
+    Function Get-Build {
+        $ReleaseID = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId
+        $DisplayVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name DisplayVersion).DisplayVersion
+        If ($ReleaseID -eq '2009') {
+            Return $DisplayVersion
+        }
+        Else {
+            Return $ReleaseID
+        }
+    }
 
     $CurrentBuild = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentBuild).CurrentBuild
     $UBR = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name UBR).UBR
-    Return $CurrentBuild + '.' + $UBR
+    $CurrentOSBuild = $CurrentBuild + '.' + $UBR
+
+    If ($Detailed -eq $true) {
+        Try {
+            $GetOSCaption = (Get-CIMInstance win32_operatingsystem).Caption
+        }
+        Catch {
+            Throw "Unable to get operating system caption. If you believe this is incorrect please submit an issue at https://github.com/AshleyHow/WindowsOSBuild/issues and include the following info :- `nOS Caption: $GetOSCaption, Error: $($_.Exception.Message)"
+        }
+
+        If ($GetOSCaption -match "Windows 10") {
+            $DetectedOS = "Win10"
+        }
+        ElseIf ($GetOSCaption -match "Windows 11") {
+            $DetectedOS = "Win10"
+        }
+        ElseIf ($GetOSCaption -match "Server 2016") {
+            $DetectedOS = "Server2016"
+        }
+        ElseIf ($GetOSCaption -match "Server 2019") {
+            $DetectedOS = "Server2019"
+        }
+        ElseIf ($GetOSCaption -match "Server 2022") {
+            $DetectedOS = "Server2022"
+        }
+        ElseIf ($GetOSCaption -match "Windows Server Standard" -or $GetOSCaption -match "Windows Server Datacenter") {
+            $DetectedOS = "ServerSAC"
+        }
+        ElseIf ($GetOSCaption -match "Windows Server Datacenter") {
+            $DetectedOS = "ServerSAC"
+        }
+        Else {
+            Throw "Unable to detect operating system. If you believe this is incorrect please submit an issue at https://github.com/AshleyHow/WindowsOSBuild/issues and include the following info :- `nOS Caption: $GetOSCaption, Detected OS: $GetOSCaption"
+        }
+
+        Get-LatestOSBuild -OSName $DetectedOS -OSversion $(Get-Build) -LatestReleases 1000 | Where-Object -Property Build -eq $CurrentOSBuild
+    }
+    Else {
+        Return $CurrentOSBuild
+    }
 }
